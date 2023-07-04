a32a6d166363e7f9a9df079fb304f868f6276b27
Aleksey Yakovlev <aleksey.yakovlev@agoda.com>
2023-07-04 14:28:59 +0700

    Initial commit

---
+++ src/main/kotlin/io/github/lexadiky/cyan/ConventionLibraryJvmPlugin.kt
@@ -0,0 +1,23 @@
+package io.github.lexadiky.cyan
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.extra
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+
+class ConventionLibraryJvmPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.plugins.apply("org.jetbrains.kotlin.jvm")
+        target.plugins.apply("org.jetbrains.kotlinx.kover")
+
+        target.extensions.configure<KotlinProjectExtension> {
+            jvmToolchain(resolveJvmToolchainVersion(target))
+        }
+    }
+
+    private fun resolveJvmToolchainVersion(target: Project): Int {
+        return target.extra["cyan.kotlin.jvm-toolchain"].toString().toInt()
+    }
+}
